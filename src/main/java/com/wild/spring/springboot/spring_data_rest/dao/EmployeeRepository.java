5b3040bc2afcae56d0a7f1bb84f01bbca576ef24
AndreyWild <and1rei@mail.ru>
2021-04-27 20:46:53 +0300

    create project

---
+++ src/main/java/com/wild/spring/springboot/spring_data_rest/dao/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.wild.spring.springboot.spring_data_rest.dao;
+
+
+
+import com.wild.spring.springboot.spring_data_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
